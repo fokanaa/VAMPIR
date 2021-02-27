777f5315035a85231cfa6b5bbb6be7a79f5523bf
VAMPIR <khabursky@gmail.com>
2021-02-27 16:28:20 +0200

    вивожу малюнок в систему

--- src/MMO.java
+++ src/MMO.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MMO {
+public class MMO {
+    public static void main(String[] args) {
+        System.out.println("(\\_/)\n" +
+                "(•x•)\n" +
+                "(<\uD83C\uDF66)");
+        System.out.println("♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪\n" +
+                "░H░A░P░P░Y░♪░B░I░R░T░H░D░A░Y░\n" +
+                "♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪");
+    }
 }
