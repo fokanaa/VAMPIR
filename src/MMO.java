cf752932830a64a5adba62931bc9836312ae2c79
VAMPIR <khabursky@gmail.com>
2021-02-27 14:23:50 +0200

    Добавлений файл Початку

--- src/MMO.java
+++ src/MMO.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class MMO {
+import java.util.function.DoubleToIntFunction;
+
+public class MMO{
+    public static void main(String[] arge){
+        int[] windows = new int[4];
+        int[] dors = new int[2];
+        int[] floor = new int[1];
+        System.out.print("Це перший запуск");
+}
+
 }
