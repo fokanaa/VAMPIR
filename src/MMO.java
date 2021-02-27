41782317ce05f467b910a7717bc268b6a97afca2
VAMPIR <khabursky@gmail.com>
2021-02-27 14:25:48 +0200

    Добавив начинаю 2

--- src/MMO.java
+++ src/MMO.java
@@ -1,11 +1,2 @@
-import java.util.function.DoubleToIntFunction;
-
-public class MMO{
-    public static void main(String[] arge){
-        int[] windows = new int[4];
-        int[] dors = new int[2];
-        int[] floor = new int[1];
-        System.out.print("Це перший запуск");
-}
-
+package PACKAGE_NAME;public class MMO {
 }
